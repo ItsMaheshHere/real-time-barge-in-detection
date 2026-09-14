0455f17bc41efcc79ae8f5a31ca88e5c6afa73f4
mah3shbishnoi <maheshsaran477@gmail.com>
2026-09-14 18:38:04 +0530

    add stop_event support to BargeInDetector for streaming interruption

--- src/bargein/bargein.py
+++ src/bargein/bargein.py
@@ -7,21 +7,26 @@ class BargeInDetector:
         self.tts = tts_instance
         self.vad_filter = vad_filter_instance
         self.is_ai_speaking = False
-        
-    def start_ai_speech(self):
+        self._stop_event = None
+
+    def start_ai_speech(self, stop_event=None):
         self.is_ai_speaking = True
+        self._stop_event = stop_event
         self.vad_filter.reset()
-        
+
     def stop_ai_speech(self):
         self.is_ai_speaking = False
-        
+        self._stop_event = None
+
     def check_barge_in(self, is_speech_confirmed: bool) -> bool:
         if not self.is_ai_speaking:
             return False
         if is_speech_confirmed:
             print("\nBarge-In: interrupted!")
             self.tts.stop()
+            if self._stop_event:
+                self._stop_event.set()
             self.is_ai_speaking = False
             return True
-            
+
         return False
